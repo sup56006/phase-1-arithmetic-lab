3b4e5754ebffd9299d1354d040864f5502421866
Isaac Kyuma <isaackyuma56@gmail.com>
2025-09-09 08:51:43 +0300

    Complete arithmetic lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// Provided variables for multiply
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+// Multiply num1 and num2 to get 62
+const multiply = num1 * num2;
+
+// Generate a random integer > 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Variables for mod
+const num3 = 28;
+const num4 = 6;
+
+// Find remainder of num3 divided by num4
+const mod = num3 % num4;
+
+// Find max value in a set
+const max = Math.max(5, 10, 15, 20);
